63afdd70b2a8bb3d751af210a74a68aa386e2f8c
YooJinSuh <rylie.suh35@gmail.com>
2021-03-09 03:58:38 +0900

    Modify: 리뷰반영 리팩토링

---
+++ src/Pages/Mypage/Components/MenuTab.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuTabItem from "./MenuTabItem";
+
+const MenuTab = () => {
+  const [currentMenu, setCurrentMenu] = useState(0);
+
+  const menuHandler = (id) => {
+    setCurrentMenu(id);
+  };
+
+  return (
+    <MenuTabWrapper>
+      <UserNameText>Rylie YJ Suh</UserNameText>
+      {CATEGORY_ARR.map((category, idx) => {
+        return (
+          <MenuTabItem
+            key={idx}
+            idx={idx}
+            data={category}
+            menuHandler={menuHandler}
+            currentMenu={currentMenu}
+          />
+        );
+      })}
+    </MenuTabWrapper>
+  );
+};
+
+export default MenuTab;
+
+const MenuTabWrapper = styled.aside`
+  background-color: #f5f5f6;
+  width: 260px;
+  height: 1000px;
+`;
+
+const UserNameText = styled.h2`
+  margin: 0px 0px 30px 20px;
+  padding-top: 30px;
+  font-size: 25px;
+  font-weight: bold;
+  letter-spacing: 0.03em;
+`;
+
+const CATEGORY_ARR = [
+  { id: 1, title: "Security", detail: "Two-Step Verification" },
+  {
+    id: 2,
+    title: "Buying",
+    detail: "Active Bids, In-Progress, Completed Orders",
+  },
+  {
+    id: 3,
+    title: "Selling",
+    detail: "Active Asks, In-Progress, Completed Sales",
+  },
+  { id: 4, title: "Profile", detail: "Learn what's unique to you" },
+  { id: 5, title: "Portfolio", detail: "See the value of your items" },
+  { id: 6, title: "Following", detail: "Products you're watching" },
+  { id: 7, title: "Settings", detail: "Payments, Payouts, Addresses" },
+  { id: 8, title: "Log Out", detail: "See You Again" },
+];
